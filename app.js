e2c483f5da839818dc8b3ca02eb0d52cd40b2114
devoloperwep <wepdevoloper@gmail.com>
2025-05-25 18:08:10 +0500

    .

--- app.js
+++ app.js
@@ -44,8 +44,15 @@
 // }
 
 // For1. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
-let a = 1;
-let b = 5;
-for (let i = a; i <= b; i++) {
-  console.log(i);
-}
+// let a = 1;
+// let b = 5;
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+// For2. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+// let a = 1;
+// let b = 5;
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+// }
